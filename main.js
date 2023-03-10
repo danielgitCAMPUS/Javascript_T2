bb4bd6a17a11f742dcb14cff6622b5ef81fd97a0
DanielFernandezRinconCAMPUS <hey.nogithub@gmail.com>
2023-03-09 23:53:15 -0500

    feat: operadores logicos

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-console.log(`<--- Metodo trim: se utiliza para eliminar los espacios en blanco al inicio y al final de una cadena. --->`);
-let cadena = "  Daniel Felipe Fernández Rincón  ";
+console.log(`En JavaScript, los operadores aritméticos se utilizan para realizar operaciones matemáticas en valores numéricos. Los operadores aritméticos disponibles en JavaScript son los siguientes:
 
-console.log(`Cadena: ${cadena}
-    cadena.trim(): ${cadena.trim()}    
-   
-`);
++: se utiliza para sumar dos valores
+-: se utiliza para restar un valor de otro
+*: se utiliza para multiplicar dos valores
+/: se utiliza para dividir un valor por otro
+%: se utiliza para obtener el resto de una división (operador módulo)
+**: se utiliza para calcular una potencia`);
+
+let x = 27;
+let y = 10;
+
+console.log("Suma" ,x + y); 
+console.log("Resta" ,x - y); 
+console.log("Multiplicación" ,x * y);
+console.log("Dividir" ,x / y); 
+console.log("Modulo o resto" ,x % y); 
+console.log("Potencia" ,x ** 2);
+
