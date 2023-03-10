4926c717452c07076a608792fc0ea89dda782790
DanielFernandezRinconCAMPUS <hey.nogithub@gmail.com>
2023-03-09 21:55:15 -0500

    feat: implementacion del metodo repeat

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
-console.log(` <--- Metodo matchAll: Busca todas las coincidencias de una expresión regular en una cadena dada y devuelve un objeto iterable que contiene información sobre cada coincidencia encontrada. --->`);
-let cadena = "Daniel Fernández";
-let regex = /[aeiou]/g;
-let result = cadena.matchAll(regex);
-
-
-console.log(`Cadena: ${cadena} , Regex : ${regex},
-    result = cadena.matchAll(regex), 
-    
-    el resultado es un objeto iterable que se puede recorrer con un for of
-
-    for (const match of result) {
-        console.log(match);        
-    }
-
-    Resultado: 
-    `);
-
-for (const match of result) {
-    console.log(match);
-    
-}
+console.log(`<--- Metodo repeat: Devuelve una cadena que consiste en la cadena original repetida el número de veces especificado. --->`);
+let cadena = "Daniel";
+console.log(`Cadena: ${cadena},
+    cadena.repeat(5): ${cadena.repeat(5)}
+`);
