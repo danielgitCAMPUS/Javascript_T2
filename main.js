34b0dd6d8afe13748c05597c426b1ffdb55aa74c
DanielFernandezRinconCAMPUS <hey.nogithub@gmail.com>
2023-03-09 22:03:51 -0500

    feat: implementacion del metodo slice

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-console.log(`<--- Metodo replace: Busca una cadena para una expresión regular y la reemplaza por otra cadena. --->`);
+console.log(`<--- Metodo slice: Devuelve una sección de una cadena. --->`);
 let cadena = "Daniel Fernández";
 
-let result;
-console.log(`Cadena inicial: ${cadena}
-    Reemplazar Daniel por Felipe
-    cadena.replace("Daniel", "Felipe"): ${cadena.replace("Daniel", "Felipe")}
+console.log(`Cadena: ${cadena}
+    cadena.slice(0,6): ${cadena.slice(0,6)}
+    cadena.slice(7,16): ${cadena.slice(7,16)}
 `);
