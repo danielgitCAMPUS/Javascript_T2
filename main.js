ff3c6bd17bb9b4be3b19cadb1b0765348779638c
DanielFernandezRinconCAMPUS <hey.nogithub@gmail.com>
2023-03-09 23:25:16 -0500

    feat: implementacion del metodo substring

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-console.log(`<--- Metodo substr: se utiliza para devolver una parte de una cadena, comenzando desde el índice especificado y con la longitud especificada. --->`);
+console.log(`<--- Metodo substring:  se utiliza para devolver una parte de una cadena, desde el índice especificado hasta el índice final especificado. --->`);
 let cadena = "Daniel Fernández";
 
 console.log(`Cadena: ${cadena}
-    cadena.substr(0,6): ${cadena.substr(0,6)}    
+    cadena.substring(0,10): ${cadena.substring(0,10)}    
 `);
